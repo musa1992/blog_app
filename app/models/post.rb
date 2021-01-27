038e6c0ad389390da676a6352c84f52b588aeb5b
musa <mosesbwire91@gmail.com>
2021-01-27 22:39:31 +0300

    Add validations to posts model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    VALID_TEXT_REGEX = /\w+/
+    validates :title, format: {with: VALID_TEXT_REGEX},
+                length: {maximum: 70}
+    validates :body, format: {with: VALID_TEXT_REGEX}
+    validate :minimum_word_count
+    validates :author, format: {with: VALID_TEXT_REGEX},
+              length: {maximum: 30}
+    private
+
+    def minimum_word_count
+        errors.add(:body, "words cannot be below 10") if body.split.size < 10
+    end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new(title: "Tests", body: "This is the body", author: "Am the scribe")
+  end
+  test "title should be present" do 
+    @post.title = " "
+    assert_not @post.valid?
+  end
+
+  test "body should be present" do
+    @post.body = " "
+    assert_not @post.valid?
+  end
+
+  test "author should be present" do
+    @post.author = " "
+    assert_not @post.valid?
+  end
+  test "title should not be too long" do
+    @post.title = 'a'* 71
+    assert_not @post.valid?
+  end
+
+  test "author should not be too long" do
+    @post.author = 'a' * 31
+    assert_not @post.valid?
+  end
+
+  test "body should not be too short" do
+    @post.body = Array.new(9,"blog").join(" ")
+    assert_not @post.valid?
+  end
 end
