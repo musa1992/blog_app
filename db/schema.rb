34a95db18bb3a5b51972e3e19cabfa0d0eebcb9c
musa <mosesbwire91@gmail.com>
2021-02-02 22:55:48 +0300

    Add photo url field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_200533) do
+ActiveRecord::Schema.define(version: 2021_02_02_194949) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_200533) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
   end
 
 end
